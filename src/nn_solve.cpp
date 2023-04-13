cd8044f808a638401f8c37ce4913452c4b109c44
Wayzm <wyeumo@yahoo.fr>
2023-04-13 19:12:37 +0200

    [src] Compute error value with mean squared method

--- src/nn_solve.cpp
+++ src/nn_solve.cpp
@@ -1,5 +1,22 @@
 #include "nn.h"
+#include "compute.h"
 
 template <typename T> T nn<T>::get_err(){
     return err;
+}
+
+template <typename T> void nn<T>::compute_err(){
+    const ui32 last_layer_index = nn_shape.size() - 1;
+    err = 0.0;
+    /*  MEAN SQUARED ERROR METHOD   */
+    for(ui32 i = 0U; i < nn_shape[last_layer_index]; ++i){
+        const T diff = NN_layers[last_layer_index][i] - Output[i];
+        err += diff * diff;
+        NN_layers_D[last_layer_index - 1][i] = diff * last_layer_derivative_activation(NN_layers[last_layer_index][i]);
+    }
+    err /= nn_shape[last_layer_index];
+}
+
+template <typename T> void nn<T>::propagation(){
+
 }
