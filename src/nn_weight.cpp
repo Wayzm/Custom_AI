8d707aba850c3331534fcbba95d664acc616bedb
Wayzm <wyeumo@yahoo.fr>
2023-04-13 18:52:43 +0200

    [src] Implemented inertia method for weight actualisaiton

--- src/nn_weight.cpp
+++ src/nn_weight.cpp
@@ -1,4 +1,5 @@
 #include "nn.h"
+#include "compute.h"
 
 template <typename T> void  nn<T>::set_weight_method(weight_functions selected_method){
     current_weight_method = selected_method;
@@ -39,6 +40,42 @@ template <typename T> void nn<T>::init_weight(const T min, const T max){
     for(auto &vector_b : Bias_Matrix_D)
         for(auto &element : vector_b)
             element = 0;
+}
+
+template <typename T> void nn<T>::inertie(){
+    /* CONSTRUCTION OF BLAS APPLICATION */
+    compute<T> c;
+    /* MAIN LOOP */
+    const auto last_layer_index = nn_shape.size() - 1;
+    #pragma omp parallel for schedule(dynamic, 1)
+    for(ui32 i = 0U; i < last_layer_index; ++i){
+        const std::vector<T> Old_bias_vector = Bias_Matrix[i];
+        const std::vector<T> Old_weight_vector = Weight_Matrix[i];
+
+        /*  UPDATE BIAS VALUES  */
+        for(ui32 j = 0U; j < nn_shape[i+1]; ++j){
+            const T tmp = NN_layers_D[i][j];
+            Bias_Matrix[i][j] -= learning_rate * learning_rate_inertie * tmp;
+            Bias_Matrix[i][j] += (1 - learning_rate_inertie) * Bias_Matrix_D[i][j];
+            Bias_Matrix_D[i][j] = Bias_Matrix[i][j] - Old_bias_vector[i];
+        }
+
+        for(ui32 j = 0U; j < Weight_Matrix[i].size(); ++j){
+            Weight_Matrix[i][j] *= (1 - learning_rate_inertie);
+        }
+        /*      WEIGHT MODIFICATION     */
+        c.emm(NN_layers_D[i], nn_shape[i + 1], 1,
+              NN_layers[i], 1, nn_shape[i],
+              (1 - learning_rate_inertie),
+              Weight_Matrix[i], nn_shape[i + 1], nn_shape[i], 1.0);
+        Weight_Matrix_D[i] = Weight_Matrix[i];
+        for(ui32 j = 0U; j < Old_weight_vector.size(); ++j)
+            Weight_Matrix_D[i][j] -= Old_weight_vector[j];
+    }
+}
+
+template <typename T> void nn<T>::standard(){
+
 }
 
 template <typename T> void nn<T>::update_weight(){
