c3a45907145357ef92d7836d4f7008abe8621d1a
Wayzm <wyeumo@yahoo.fr>
2023-04-13 18:52:43 +0200

    [src] Implemented new constructors

--- src/nn.cpp
+++ src/nn.cpp
@@ -9,7 +9,7 @@ template <typename T> nn<T>::nn(){
     learning_rate = 0.1;
     learning_rate_inertie = 0.05;
     set_shape();
-    shape();
+    shaping();
     init_weight();
 }
 
@@ -21,7 +21,7 @@ template <typename T> nn<T>::nn(const std::vector<ui32> nn_shape,
     last_layer_current_activation_function = linear;
     learning_rate = 0.1;
     learning_rate_inertie = 0.05;
-    shape();
+    shaping();
     init_weight();
 }
 
@@ -33,7 +33,34 @@ template <typename T> nn<T>::nn(const std::vector<ui32> nn_shape,
     last_layer_current_activation_function = linear;
     learning_rate = 0.1;
     learning_rate_inertie = 0.05;
-    shape();
+    shaping();
+    init_weight();
+}
+
+template <typename T> nn<T>::nn(const std::vector<ui32> nn_shape,
+                                const activation_functions act_function,
+                                const weight_functions weight_method){
+    err = {};
+    current_weight_method = weight_method;
+    current_activation_function = act_function;
+    last_layer_current_activation_function = linear;
+    learning_rate = 0.1;
+    learning_rate_inertie = 0.05;
+    shaping();
+    init_weight();
+}
+
+template <typename T> nn<T>::nn(const std::vector<ui32> nn_shape,
+                                const activation_functions act_function,
+                                const activation_functions last_layer_act_function,
+                                const weight_functions weight_method){
+    err = {};
+    current_weight_method = weight_method;
+    current_activation_function = act_function;
+    last_layer_current_activation_function = last_layer_act_function;
+    learning_rate = 0.1;
+    learning_rate_inertie = 0.05;
+    shaping();
     init_weight();
 }
 
